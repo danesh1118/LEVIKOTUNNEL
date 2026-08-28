e1fb1cce79dff9ac4b0ad33c684b460640db898c
danesh1118 <daneshvaisi1118@gmail.com>
2026-08-28 11:17:54 +0330

    Add files via upload

---
+++ internal/utils/handlers/tcp_handler.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net"
+
+	"github.com/danesh1118/LEVIKOTUNNEL/internal/web"
+	"github.com/sirupsen/logrus"
+)
+
+func TCPConnectionHandler(ctx context.Context, proxyProtocol bool, from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
+	done := make(chan struct{})
+
+	// Write Proxy Protocol V2 Header
+	if proxyProtocol {
+		err := WriteProxyProtocol(from, to)
+		if err != nil {
+			logger.Error(err)
+			from.Close()
+			to.Close()
+			return
+		}
+	}
+
+	go func() {
+		defer close(done)
+		transferData(from, to, logger, usage, remotePort, sniffer)
+	}()
+
+	transferData(to, from, logger, usage, remotePort, sniffer)
+
+	select {
+	case <-ctx.Done():
+		from.Close()
+		to.Close()
+		return
+	case <-done:
+	}
+}
+
+// Using direct Read and Write for transferring data
+func transferData(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
+	buf := make([]byte, 16*1024) // 16K
+	for {
+		// Read data from the source connection
+		r, err := from.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
+				logger.Trace("reader stream closed or EOF received")
+			} else {
+				logger.Trace("unable to read from the connection: ", err)
+			}
+			from.Close()
+			to.Close()
+			return
+		}
+
+		totalWritten := 0
+		for totalWritten < r {
+			// Write data to the destination connection
+			w, err := to.Write(buf[totalWritten:r])
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
+					logger.Trace("writer stream closed or EOF received")
+				} else {
+					logger.Trace("unable to write to the connection: ", err)
+				}
+				from.Close()
+				to.Close()
+				return
+
+			}
+			totalWritten += w
+		}
+
+		logger.Tracef("read data: %d bytes, written data: %d bytes", r, totalWritten)
+		if sniffer {
+			usage.AddOrUpdatePort(remotePort, uint64(totalWritten))
+		}
+	}
+
+}
