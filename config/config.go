6015f3ee81825616dd86921a18c675e2198a88be
danesh1118 <daneshvaisi1118@gmail.com>
2026-08-28 11:07:28 +0330

    Add files via upload

--- config/config.go
+++ config/config.go
@@ -1,1 +1,79 @@
-0
+package config
+
+// TransportType defines the type of transport.
+type TransportType string
+
+const (
+	TCP    TransportType = "tcp"
+	TCPMUX TransportType = "tcpmux"
+	WS     TransportType = "ws"
+	WSS    TransportType = "wss"
+	WSMUX  TransportType = "wsmux"
+	WSSMUX TransportType = "wssmux"
+	UDP    TransportType = "udp"
+)
+
+// ServerConfig represents the configuration for the server.
+type ServerConfig struct {
+	BindAddr         string        `toml:"bind_addr"`
+	Transport        TransportType `toml:"transport"`
+	Token            string        `toml:"token"`
+	Nodelay          bool          `toml:"nodelay"`
+	Keepalive        int           `toml:"keepalive_period"`
+	ChannelSize      int           `toml:"channel_size"`
+	LogLevel         string        `toml:"log_level"`
+	Ports            []string      `toml:"ports"`
+	PPROF            bool          `toml:"pprof"`
+	MuxSession       int           `toml:"mux_session"`
+	MuxVersion       int           `toml:"mux_version"`
+	MaxFrameSize     int           `toml:"mux_framesize"`
+	MaxReceiveBuffer int           `toml:"mux_recievebuffer"`
+	MaxStreamBuffer  int           `toml:"mux_streambuffer"`
+	Sniffer          bool          `toml:"sniffer"`
+	WebPort          int           `toml:"web_port"`
+	SnifferLog       string        `toml:"sniffer_log"`
+	TLSCertFile      string        `toml:"tls_cert"`
+	TLSKeyFile       string        `toml:"tls_key"`
+	Heartbeat        int           `toml:"heartbeat"`
+	MuxCon           int           `toml:"mux_con"`
+	AcceptUDP        bool          `toml:"accept_udp"`
+	SkipOptz         bool          `toml:"skip_optz"`
+	MSS              int           `toml:"mss"`
+	SO_RCVBUF        int           `toml:"so_rcvbuf"`
+	SO_SNDBUF        int           `toml:"so_sndbuf"`
+	ProxyProtocol    bool          `toml:"proxy_protocol"`
+}
+
+// ClientConfig represents the configuration for the client.
+type ClientConfig struct {
+	RemoteAddr       string        `toml:"remote_addr"`
+	Transport        TransportType `toml:"transport"`
+	Token            string        `toml:"token"`
+	ConnectionPool   int           `toml:"connection_pool"`
+	RetryInterval    int           `toml:"retry_interval"`
+	Nodelay          bool          `toml:"nodelay"`
+	Keepalive        int           `toml:"keepalive_period"`
+	LogLevel         string        `toml:"log_level"`
+	PPROF            bool          `toml:"pprof"`
+	MuxSession       int           `toml:"mux_session"`
+	MuxVersion       int           `toml:"mux_version"`
+	MaxFrameSize     int           `toml:"mux_framesize"`
+	MaxReceiveBuffer int           `toml:"mux_recievebuffer"`
+	MaxStreamBuffer  int           `toml:"mux_streambuffer"`
+	Sniffer          bool          `toml:"sniffer"`
+	WebPort          int           `toml:"web_port"`
+	SnifferLog       string        `toml:"sniffer_log"`
+	DialTimeout      int           `toml:"dial_timeout"`
+	AggressivePool   bool          `toml:"aggressive_pool"`
+	EdgeIP           string        `toml:"edge_ip"`
+	SkipOptz         bool          `toml:"skip_optz"`
+	MSS              int           `toml:"mss"`
+	SO_RCVBUF        int           `toml:"so_rcvbuf"`
+	SO_SNDBUF        int           `toml:"so_sndbuf"`
+}
+
+// Config represents the complete configuration, including both server and client settings.
+type Config struct {
+	Server ServerConfig `toml:"server"`
+	Client ClientConfig `toml:"client"`
+}
